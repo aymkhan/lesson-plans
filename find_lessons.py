8f944accffe201a00256912d316dedc163705b5d
Aymen K <aymen@Aymens-MacBook-Air.local>
2025-12-07 02:15:02 -0800

    added functions to take user input and find the requested lesson

--- find_lessons.py
+++ find_lessons.py
@@ -6,7 +6,7 @@
 import os
 import csv
 import pandas as pd
-from docx import Document
+#from docx import Document
 
 def start():
 	print("Hello, Gianna!")
@@ -17,12 +17,55 @@ ASL1_LESSONS = {}
 
 ASL1_file = os.getcwd() + "/database/lessons/ASL1_Lessons.csv"
 
+# Open the CSV file and load it into pandas by calling the read_csv() method
 with open(ASL1_file,'r') as asl1:
-	data = pd.DataFrame(asl1)
-	print(data)
-#	for column in asl1:
+	data = pd.read_csv(asl1) # read_csv() method is needed for pandas to READ the csv
+				 # print this method's value without using to_string() will only print the first few rows and last few rows
+#	print(data.to_string())
+#	print(data)
+	data.columns = data.columns.str.lower() # just to be cautious of case sensitive text, I've changed all column names to lowercase 
+#	print(data.head(10))
+
+def get_user_prompt():
+	prompt = input("📣 How can I help you?\n")
+	return prompt
+
+# Take user input 
+prompt = get_user_prompt().lower()
+
+lesson = "lesson"
+activity = "activity"
+ASL1 = "asl1"
+ASL2 = "asl2"
+ASL3 = "asl3"
+
+
+if lesson in prompt:
+	if ASL1 in prompt:
+		# split the prompt str and only keep the part from 'lesson' onwards
+		# send this part of the string to the DataFrame obj: data[str] to receive all the rows associated with this column
+		find_lesson = prompt.split(ASL1 + " ")
+		lesson = find_lesson[-1]
+		print("Here are the Vocab words for", ASL1.upper() + " " + lesson.capitalize() + ":\n" )
+		for row in data[lesson]:
+			if pd.notna(row):
+				print(row)
+			else:
+				pass
+		print("\n")
+	elif ASL2 in prompt:
+		# find ASL2 Lesson
+		print("Hello this is ASL2")
+	else:
+		print("I can help you find Lessons or Activities for ASL1, ASL2, or ASL3")
+		get_user_prompt()
+		 
+
+
+#	for column in data:
 #		if "Lesson 2" in column:
 #			print(column)
+#			print(columna
 
 
 '''
--- main.py
+++ main.py
@@ -6,15 +6,19 @@ This program returns the lesson plans for ASL 1, 2, and 3.
 	1. Download the clean/final versions of the .docx files and store them in /database
 		a. /database/lessons
 		b. /database/activity
-	2. Read the .docx file for the Lessons (might be 3 files for each class - ASL1, 2, and 3
+	2. Read the .csv file for the Lessons (might be 3 files for each class - ASL1, 2, and 3)
+		NOTE: The original file written by the teacher is in .docs format.
+		Create a new .csv file for better readability. 
+		Each column will be a Lesson # and the rows will consist of the Vocab words.		
 	3. Read the .docx file for Activities (single file) 
 
-DOCX files:
-	1. LESSONS
-		- Create a Dict to load into pandas DataFrame per course: ASL1, ASL2, and ASL3
-		- Dict should have Lesson numbers as KEYS, for ex: "Lesson 1", "Lesson 1.2", "Lesson 2" 
-		- Dict should have the vocab words per lesson as VALUES
-		- Example Dict:
+CSV and DOCX files:
+	1. LESSONS (CSV)
+		- Load the CSV file into pandas DataFrame per course: ASL1, ASL2, and ASL3
+		- Let pandas READ the .csv file by calling the read_csv() method
+#		- Dict should have Lesson numbers as KEYS, for ex: "Lesson 1", "Lesson 1.2", "Lesson 2" 
+#		- Dict should have the vocab words per lesson as VALUES
+#		- Example Dict:
 			ASL1 = {
 				"Lesson 1":["hello", "good morning", "good night"],
 				"Lesson 1.2":["good evening", "bonjour"],
@@ -23,7 +27,7 @@ DOCX files:
 
 USER INPUT TO READ THE LESSONS:
 	1. Take user input for READING lessons:
-		- Maybe allow the user to enter something like: "ASL1 lesson 2" for Lessons and "ASL1 activity 3" for Activities
+		- Maybe allow the user to enter a prompt, something like: "ASL1 lesson 2" for Lessons and "ASL1 activity 3" for Activities
 		- If the user response contains "ASL1", check if the response contains "lesson" 
 
 USER INPUT TO WRITE NEW LESSONS:
